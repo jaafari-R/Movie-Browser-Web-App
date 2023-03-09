c823c07d6e8d6c32f5bc211c029158c6e448e469
potato <jaafariramadan@gmail.com>
2023-03-09 21:20:17 +0200

    Added Home, About & Search components & App a React Route for each

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Search from './components/Search';
 
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Navbar />
+
+      <BrowserRouter>
+        <Navbar />
+        
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/search" element={<Search />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
